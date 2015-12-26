3730b99abfa0c4ac91f7d3be054ca2d25fd99886
Pietro <pietro.passarelli@gmail.com>
2015-12-26 04:43:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
+// This is a modified version of say.js by Marak Squires http://github.com/marak/say.js/
+
+/*
+Copyright (c) 2010 Marak Squires http://github.com/marak/say.js/
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 'use strict';
-
 var spawn = require('child_process').spawn,
-  child;
-
+var speechProcess;
 var say = exports;
 
 if (process.platform === 'darwin') {
@@ -37,10 +56,10 @@ exports.speak = function(voice, text, callback, speed) {
   }
 
 
-  var childD = spawn(say.speaker, commands);
+ speechProcess = spawn(say.speaker, commands);
+  speechProcess.stdin.setEncoding('ascii');
+  speechProcess.stderr.setEncoding('ascii');
 
-  childD.stdin.setEncoding('ascii');
-  childD.stderr.setEncoding('ascii');
 
   if (process.platform === 'linux') {
     childD.stdin.end(pipedData);
@@ -51,31 +70,19 @@ exports.speak = function(voice, text, callback, speed) {
   childD.stdout.on('data', function(data){ console.log(data); });
 
 
-  childD.addListener('exit', function (code, signal) {
-    if (code === null || signal !== null) {
+ speechProcess.addListener('exit', function (code, signal) {
+    if(code == null || signal != null) {
       console.log('couldnt talk, had an error ' + '[code: '+ code + '] ' + '[signal: ' + signal + ']');
     }
 
-    ////STOP TEST
-     child = null;
-    //
-    // handle callback if given
-    if (callback) {
-      // we could do better than a try / catch here
-      try {
-        callback();
-      } catch(err) {
-        // noop
-      }
-    }
+    if ( callback ){ callback() }
   });
-};
+}
 
-//STOP TEST
-exports.stop = function () {
-  if (!child) {
-    return;
-  }
-  child.stdin.pause();
-  child.kill();
+
+// this stop function from a pull request on say.js (https://github.com/lakenen/say.js/blob/stop/lib/say.js)
+exports.stop = function(){
+  if (!speechProcess){ return; }
+  speechProcess.stdin.pause();
+  speechProcess.kill();
 }

581f4f9fdd962c42fc9f836988d4c2033dc40027
Till Affeldt <till.affeldt@gmail.com>
2017-09-07 15:19:40 +0200

    Add TypeScript support
    I added a definition file for use with TypeScript that provides
    information on the available functions.

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+declare module 'say' {
+  const say: SayJS.Say;
+
+  namespace SayJS {
+    type errorCallback = (err: string) => void;
+
+    class Say {
+      public export(text: string, voice?: string, speed?: number, filePath?: string, callback?: errorCallback): void;
+      public speak(text: string, voice?: string, speed?: number, callback?: errorCallback): void;
+      public stop(): void;
+    }
+  }
+
+  export = say;
+}

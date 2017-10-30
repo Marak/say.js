919b7bbee353cfe39b8098a21d3b4e416b1a981d
Jim Buck <jim@jimmyboh.com>
2017-10-30 18:24:44 -0400

    Removed code from separate PR.

--- platform/win32.js
+++ platform/win32.js
@@ -18,11 +18,6 @@ class SayPlatformWin32 extends SayPlatformBase {
 
     let psCommand = `Add-Type -AssemblyName System.speech;$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer;`
 
-    if (speed) {
-      let adjustedSpeed = this.convertSpeed(speed || 1)
-      psCommand += `$speak.Rate = ${adjustedSpeed};`
-    }
-
     if (voice) {
       psCommand += `$speak.SelectVoice('${voice}');`
     }
@@ -43,11 +38,6 @@ class SayPlatformWin32 extends SayPlatformBase {
   runStopCommand () {
     this.child.stdin.pause()
     childProcess.exec(`taskkill /pid ${this.child.pid} /T /F`)
-  }
-
-  convertSpeed (speed) {
-    // Overriden to map playback speed (as a ratio) to Window's values (-10 to 10, zero meaning x1.0)
-    return Math.max(-10, Math.min(Math.round((9.0686 * Math.log(speed)) - 0.1806), 10))
   }
 }
 
